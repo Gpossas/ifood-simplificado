a0c79e2e21aa4f9a57ede8291ccde50efecd0f52
gpossas <possasg@gmail.com>
2025-03-04 18:34:04 -0300

    feat: add sqs service
    - add sqs dependency
    - config sqs client

---
+++ orders_microservice/src/main/java/com/guipossas/orders/config/AWSConfig.java
@@ -0,0 +1,15 @@
+package com.guipossas.orders.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.services.sqs.SqsClient;
+
+@Configuration
+public class AWSConfig
+{
+    @Bean
+    public SqsClient sqs()
+    {
+        return SqsClient.builder().build();
+    }
+}
---
+++ orders_microservice/src/main/java/com/guipossas/orders/services/SqsService.java
@@ -0,0 +1,19 @@
+package com.guipossas.orders.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+import software.amazon.awssdk.services.sqs.model.SendMessageResponse;
+
+@Service
+@RequiredArgsConstructor
+public class SqsService
+{
+    private final SqsClient sqsClient;
+
+    public SendMessageResponse publishMessage(SendMessageRequest sendMessageRequest)
+    {
+        return sqsClient.sendMessage(sendMessageRequest);
+    }
+}

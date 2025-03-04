39e68ff3594a8ac600431700d96e417d0ecd83d6
gpossas <possasg@gmail.com>
2025-03-04 15:58:31 -0300

    style: fix no punctuation in exception message

--- orders_microservice/src/main/java/com/guipossas/orders/exceptions/OrderWithEmptyItems.java
+++ orders_microservice/src/main/java/com/guipossas/orders/exceptions/OrderWithEmptyItems.java
@@ -2,6 +2,6 @@ package com.guipossas.orders.exceptions;
 
 public class OrderWithEmptyItems extends RuntimeException {
     public OrderWithEmptyItems() {
-        super("O pedido nao pode estar vazio");
+        super("O pedido não pode estar vazio");
     }
 }

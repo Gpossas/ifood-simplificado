bdd96a912344cadd2d53c32fbc2efdafae2e7611
gpossas <possasg@gmail.com>
2025-03-04 08:02:30 -0300

    fix: missing repository annotation

--- orders_microservice/src/main/java/com/guipossas/orders/repository/OrderItemRepository.java
+++ orders_microservice/src/main/java/com/guipossas/orders/repository/OrderItemRepository.java
@@ -2,6 +2,8 @@ package com.guipossas.orders.repository;
 
 import com.guipossas.orders.domain.OrderItem;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderItemRepository extends MongoRepository<OrderItem, String> {
 }

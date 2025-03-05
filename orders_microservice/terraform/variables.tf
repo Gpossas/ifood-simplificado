c85c0de9c01f8280eb2786db6b87456f41e71ffa
gpossas <possasg@gmail.com>
2025-03-05 19:51:02 -0300

    ops: add terraform vpc module

--- orders_microservice/terraform/variables.tf
+++ orders_microservice/terraform/variables.tf
@@ -1,13 +1,13 @@
 variable "database_name" {
   description = "Name of the database"
-  type    = string
-  default = "ifood-simplificado-db"
+  type        = string
+  default     = "ifood-simplificado-db"
 }
 
 variable "orders_placed_queue_name" {
   description = "Name of the queue for orders placed"
-  type    = string
-  default = "orders-placed-queue.fifo"
+  type        = string
+  default     = "orders-placed-queue.fifo"
 }
 
 variable "receive_message_wait_time" {
@@ -24,8 +24,8 @@ variable "message_retention_period_seconds" {
 
 variable "account_id" {
   description = "AWS account id"
-  type     = string
-  nullable = false
+  type        = string
+  nullable    = false
 }
 
 variable "project_tags" {
@@ -35,4 +35,34 @@ variable "project_tags" {
     terraform   = "true"
     environment = "dev"
   }
+}
+
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+  default     = "ifood-simplificado-vpc"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR of the VPC"
+  type        = string
+  default     = "10.0.1.0/26"
+}
+
+variable "vpc_azs" {
+  description = "Availability zones of the VPC"
+  type        = list(string)
+  default     = ["sa-east-1a", "sa-east-1b"]
+}
+
+variable "vpc_private_subnets" {
+  description = "Private subnets of the VPC"
+  type        = set(string)
+  default     = ["10.0.1.0/28", "10.0.1.16/28"]
+}
+
+variable "vpc_public_subnets" {
+  description = "Public subnets of the VPC"
+  type        = set(string)
+  default     = ["10.0.1.32/28", "10.0.1.48/28"]
 }

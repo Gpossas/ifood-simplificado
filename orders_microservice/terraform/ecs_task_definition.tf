2579c3c4f764fb8a7a1d45b0b1b6f631039b3baa
gpossas <possasg@gmail.com>
2025-03-06 16:57:26 -0300

    ops: add ecs task definition

---
+++ orders_microservice/terraform/ecs_task_definition.tf
@@ -0,0 +1,37 @@
+resource "aws_ecs_task_definition" "ecs_task_definition" {
+    family                   = "${var.project_tags.project}-task-family"
+    network_mode             = "awsvpc"
+    requires_compatibilities = ["FARGATE"]
+    cpu                      = var.task_definition_cpu
+    memory                   = var.task_definition_memory
+    execution_role_arn       = aws_iam_role.task_execution_role.arn
+    task_role_arn            = aws_iam_role.ecs_task_role.arn
+    container_definitions    = jsonencode([
+      {
+        name: var.microservice_name,
+        image: var.task_definition_microservice_image,
+        essential: true,
+        portMappings: [
+          {
+            "containerPort": 8080,
+            "hostPort": 8080
+          }
+        ],
+        logConfiguration: {
+          "logDriver": "awslogs",
+          "options": {
+            "awslogs-group": "/ecs/${var.microservice_name}",
+            "awslogs-region": var.region,
+            "awslogs-stream-prefix": var.project_tags.project
+          }
+        }
+      }
+    ])
+
+  tags = var.project_tags
+}
+
+data "aws_ecs_container_definition" "ecs_container_definition" {
+  task_definition = aws_ecs_task_definition.ecs_task_definition.arn
+  container_name = var.microservice_name
+}
---
+++ orders_microservice/terraform/iam.tf
@@ -0,0 +1,44 @@
+# ECS task execution role
+resource "aws_iam_role" "task_execution_role" {
+  name               = "${var.project_tags.project}-task-execution-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_trusted_entities_policy.json
+}
+data "aws_iam_policy_document" "ecs_task_trusted_entities_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+resource "aws_iam_role_policy_attachment" "task_execution_role_policy" {
+  role       = aws_iam_role.task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# ECS task role
+resource "aws_iam_role" "ecs_task_role" {
+  name               = "${var.project_tags.project}-ecs-task-role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_trusted_entities_policy.json
+  description = "Allows ECS tasks to call AWS services on your behalf."
+  tags = var.project_tags.project
+}
+resource "aws_iam_role_policy_attachment" "attach_sqs_policy" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.sqs_send_message_policy.arn
+}
+resource "aws_iam_policy" "sqs_send_message_policy" {
+  name   = "${var.project_tags.project}-sqs-send-message-policy"
+  policy = data.aws_iam_policy_document.sqs_send_message_policy.json
+  description = "Allows to send messages to SQS"
+}
+data "aws_iam_policy_document" "sqs_send_message_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sqs:SendMessage"
+    ]
+    resources = [aws_sqs_queue.orders-placed-queue.arn]
+  }
+}

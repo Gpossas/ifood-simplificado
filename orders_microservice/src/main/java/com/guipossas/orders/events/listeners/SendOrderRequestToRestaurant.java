cd7fe976f794ead934df45f5c89175ff4d635614
gpossas <possasg@gmail.com>
2025-03-10 19:49:06 -0300

    fix(SendOrderRequestToRestaurant):  event subscriber doesn't trigger. Add event listener annotation

--- orders_microservice/src/main/java/com/guipossas/orders/events/listeners/SendOrderRequestToRestaurant.java
+++ orders_microservice/src/main/java/com/guipossas/orders/events/listeners/SendOrderRequestToRestaurant.java
@@ -4,17 +4,24 @@ import com.guipossas.orders.events.OrderPlacedEvent;
 import com.guipossas.orders.factories.OrderQueueRequest;
 import com.guipossas.orders.services.SqsService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class SendOrderRequestToRestaurant
 {
     private final SqsService sqsService;
     private final OrderQueueRequest orderQueueRequest;
 
+    @EventListener
     public void onOrderPlaced(OrderPlacedEvent orderPlacedEvent)
     {
+        log.info("Subscriber {} received order placed event {}", this.getClass().getSimpleName(), orderPlacedEvent.getOrder().getOrderNumber());
         sqsService.publishMessage(orderQueueRequest.createMessageRequest(orderPlacedEvent.getOrder()));
+        log.info("Sent order {} to restaurant", orderPlacedEvent.getOrder().getOrderNumber());
     }
 }

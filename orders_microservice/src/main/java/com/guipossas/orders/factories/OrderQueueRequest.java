2e97b4d778114f45d797daa18e9bba1596727135
gpossas <possasg@gmail.com>
2025-03-10 19:51:04 -0300

    fix(OrderQueueRequest): message not in JSON format to SQS. Update to write string as JSON

--- orders_microservice/src/main/java/com/guipossas/orders/factories/OrderQueueRequest.java
+++ orders_microservice/src/main/java/com/guipossas/orders/factories/OrderQueueRequest.java
@@ -1,5 +1,8 @@
 package com.guipossas.orders.factories;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.guipossas.orders.domain.Order;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -13,11 +16,20 @@ public class OrderQueueRequest
 
     public SendMessageRequest createMessageRequest(Order order)
     {
-        return SendMessageRequest.builder()
-                .queueUrl(queueUrl)
-                .messageBody(order.toString())
-                .messageGroupId("orders-ms")
-                .messageDeduplicationId(order.getOrderNumber())
-                .build();
+        try
+        {
+            String message = new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(order);
+
+            return SendMessageRequest.builder()
+                    .queueUrl(queueUrl)
+                    .messageBody(message)
+                    .messageGroupId("orders-ms")
+                    .messageDeduplicationId(order.getOrderNumber())
+                    .build();
+        }
+        catch (JsonProcessingException e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 }

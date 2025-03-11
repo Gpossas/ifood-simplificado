b0e89fcd8fb6d0190202aef5f196f8906a338e0f
gpossas <possasg@gmail.com>
2025-03-11 12:38:39 -0300

    ops(terraform): add terraform infrastructure

---
+++ restaurant_microservice/terraform/vpc.tf
@@ -0,0 +1,16 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
+
+  tags = var.project_tags
+}

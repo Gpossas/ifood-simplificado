b35e6e069e41f0888918693723afead19c4c89da
gpossas <possasg@gmail.com>
2025-03-13 20:55:41 -0300

    ops(terraform): add dynamodb

--- orders_microservice/terraform/dynamodb.tf
+++ orders_microservice/terraform/dynamodb.tf
@@ -2,10 +2,28 @@ resource "aws_dynamodb_table" "orders_database_table" {
   name = var.orders_database_table
   billing_mode = "PAY_PER_REQUEST"
   hash_key = "id"
+  range_key = "createdAt"
 
   attribute {
     name = "id"
     type = "S"
+  }
+
+  attribute {
+    name = "status"
+    type = "S"
+  }
+
+  attribute {
+    name = "createdAt"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name            = "StatusCreatedAtIndex"
+    hash_key        = "status"
+    range_key       = "createdAt"
+    projection_type = "ALL"
   }
 
   tags = var.project_tags

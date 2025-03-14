56501b4913a0d6f3bc5f6179f348496c54bf1e41
gpossas <possasg@gmail.com>
2025-03-14 19:35:47 -0300

    chore: remove mongodb

--- orders_microservice/src/main/java/com/guipossas/orders/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.guipossas.orders.repository;
-
-import com.guipossas.orders.domain.Order;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Deprecated(since = "Use DynamoDbTemplate instead")
-@Repository
-public interface OrderRepository extends MongoRepository<Order, String> {}

9f6951f78572162e3f75d3f1ff7bc67959f04db5
gpossas <possasg@gmail.com>
2025-03-19 20:02:51 -0300

    ops(terraform): add dns outputs to be used in api gateway as http integration

--- terraform/environments/dev/load_balancer/outputs.tf
+++ terraform/environments/dev/load_balancer/outputs.tf
@@ -5,3 +5,11 @@ output "ecs_target_group_orders_microservice_arn" {
 output "ecs_target_group_restaurant_microservice_arn" {
   value = module.load_balancer_restaurant_microservice.ecs_target_group_arn
 }
+
+output "orders_microservice_dns_name" {
+  value = module.load_balancer_orders_microservice.alb_dns_name
+}
+
+output "restaurant_microservice_dns_name" {
+  value = module.load_balancer_restaurant_microservice.alb_dns_name
+}

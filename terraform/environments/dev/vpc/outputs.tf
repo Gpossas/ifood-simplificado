c13884db532ccbe71c2043f279b58999fda5e472
gpossas <possasg@gmail.com>
2025-03-19 09:41:37 -0300

    ops(terraform): add output public subnets to use in ecs service module and vpc id to use in load balancer module

--- terraform/environments/dev/vpc/outputs.tf
+++ terraform/environments/dev/vpc/outputs.tf
@@ -3,6 +3,12 @@ output "private_subnets" {
   value = module.vpc.private_subnets
 }
 
+output "public_subnets" {
+  description = "Set of IDs of public subnets"
+  value = module.vpc.public_subnets
+}
+
 output "vpc_id" {
+  description = "ID of the VPC"
   value = module.vpc.vpc_id
 }

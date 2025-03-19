fe56f029006f369aa131661160f7cee2650b136f
gpossas <possasg@gmail.com>
2025-03-19 13:11:19 -0300

    ops(terraform): fix task role arn output with conditional creation

--- terraform/modules/ecs_roles/outputs.tf
+++ terraform/modules/ecs_roles/outputs.tf
@@ -5,5 +5,5 @@ output "task_execution_role_arn" {
 
 output "ecs_task_role_arn" {
   description = "ARN of the ECS task role"
-  value = aws_iam_role.ecs_task_role.arn
+  value = length(aws_iam_role.ecs_task_role) > 0 ? aws_iam_role.ecs_task_role[0].arn : null
 }

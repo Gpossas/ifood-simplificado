28eeefbfbf22963c0bec58a342b3dafb670ed453
gpossas <possasg@gmail.com>
2025-03-22 09:50:08 -0300

    ops(github-actions): add terraform apply for aws resources needed to build application image

---
+++ terraform/scripts/terraform_apply_before_application_image_build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script to create resources that the applications depends on to build image (e.g. SQS urls)
+
+set -e  # Exit on first error
+
+RESOURCES=("sqs")
+
+cd "../environments/$ENV/"
+
+for RESOURCE in "${RESOURCES[@]}"; do
+  echo "Applying Terraform for $RESOURCE..."
+
+  cd "$RESOURCE/"
+
+  terraform apply -auto-approve
+
+  cd ".." # go back
+done

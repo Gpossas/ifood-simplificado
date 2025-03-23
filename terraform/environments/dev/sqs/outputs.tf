e49201f73f12de3062c4d0f2797aa8a487c958fa
gpossas <possasg@gmail.com>
2025-03-23 19:27:40 -0300

    ops(terraform): add queue url outputs to pass to build jar file

--- terraform/environments/dev/sqs/outputs.tf
+++ terraform/environments/dev/sqs/outputs.tf
@@ -7,3 +7,13 @@ output "restaurant_microservice_queue_arn" {
   description = "ARN of the restaurant microservice queue"
   value = module.restaurant_microservice_queue.queue_arn
 }
+
+output "orders_microservice_queue_url" {
+  description = "URL of the orders microservice queue"
+  value = module.orders_microservice_queue.queue_url
+}
+
+output "restaurant_microservice_queue_url" {
+  description = "URL of the restaurant microservice queue"
+  value = module.restaurant_microservice_queue.queue_url
+}
--- terraform/modules/fifo_queue/outputs.tf
+++ terraform/modules/fifo_queue/outputs.tf
@@ -1,3 +1,7 @@
 output "queue_arn" {
     value = aws_sqs_queue.fifo.arn
 }
+
+output "queue_url" {
+    value = aws_sqs_queue.fifo.url
+}

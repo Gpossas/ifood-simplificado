0acc224722715f15872b2bef98edd361f770c7ef
gpossas <possasg@gmail.com>
2025-03-23 11:45:21 -0300

    ops(github-actions): add workflow destroy and script to destroy terraform resources

---
+++ terraform/scripts/terraform_destroy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script to destroy terraform resources
+
+set -e  # Exit on first error
+
+RESOURCES=("api_gateway" "ecs" "load_balancer" "security_group" "vpc" "dynamodb" "sqs")
+
+cd "$(dirname "$0")/../environments/$ENV/"
+
+for RESOURCE in "${RESOURCES[@]}"; do
+  echo "Applying Terraform for $RESOURCE..."
+
+  cd "$RESOURCE/"
+
+  terraform destroy -auto-approve
+
+  cd ".." # go back
+done

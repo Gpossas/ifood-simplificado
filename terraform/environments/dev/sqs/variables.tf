47c59099a4d5d63b1e3e6fb1c98c5fa88115b680
gpossas <possasg@gmail.com>
2025-03-24 11:20:26 -0300

    ops(terraform): orders variables with swapped names

--- terraform/environments/dev/sqs/variables.tf
+++ terraform/environments/dev/sqs/variables.tf
@@ -1,11 +1,11 @@
 variable "order_microservice_queue_name" {
   description = "Name of the queue for orders status update"
   type = string
-  default = "orders-update-status-queue.fifo"
+  default = "orders-placed-queue.fifo"
 }
 
 variable "restaurant_microservice_queue_name" {
   description = "Name of the queue for orders placed"
   type = string
-  default = "orders-placed-queue.fifo"
+  default = "orders-update-status-queue.fifo"
 }
